097bbfac8c2c20358ee7a8043cb6a4c405514832
kubahrom <kubahromadko0506@gmail.com>
2021-04-08 00:29:25 +0200

    server app setup, register and login

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5001 });
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}`);
+  });
