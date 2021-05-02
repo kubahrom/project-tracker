224a56d2c085916fa8dc26a62adce2e6f46522c3
kubahrom <kubahromadko0506@gmail.com>
2021-05-02 23:58:14 +0200

    complete reordering issues

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,18 +1,12 @@
-import {
-  ApolloError,
-  useApolloClient,
-  useMutation,
-  useQuery,
-} from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import { CircularProgress } from '@material-ui/core';
 import React from 'react';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-import { UPDATE_BOARD_ISSUE } from '../../graphql/issuesMutation';
 import { GET_ISSUES } from '../../graphql/issuesQuery';
 import { useBoardStyles } from '../../styles/muiStyles';
 import { statusList } from '../../utils/constants';
-import { LexoRank } from 'lexorank';
 import List from './List';
+import useReorderHook from '../../utils/useReorderHook';
 
 interface IBoardProps {
   projectId: string;
@@ -46,72 +40,10 @@ const Board = ({ projectId }: IBoardProps) => {
     skip: Boolean(cachedIssues),
   });
 
-  const [updateIssue] = useMutation(UPDATE_BOARD_ISSUE, {
-    onError(err: ApolloError) {
-      console.log(err);
-    },
-  });
+  const { reorderIssue } = useReorderHook(projectId);
 
-  const handleDragEnd = ({ source, destination, draggableId }: DropResult) => {
-    if (!destination) return;
-    console.log(source, destination);
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    )
-      return;
-
-    const targetStatusIssues =
-      cachedIssues.getIssues.length !== 0
-        ? cachedIssues.getIssues
-            .filter((issue: any) => issue.status === destination.droppableId)
-            .sort((a: any, b: any) => {
-              if (a.index > b.index) return 1;
-              return -1;
-            })
-        : '';
-
-    const indexOnTarget = targetStatusIssues[destination.index]
-      ? targetStatusIssues[destination.index].index
-      : '';
-
-    const indexBeforeTarget = targetStatusIssues[destination.index - 1]
-      ? targetStatusIssues[destination.index - 1].index
-      : '';
-
-    const indexNextTarget = targetStatusIssues[destination.index + 1]
-      ? targetStatusIssues[destination.index + 1].index
-      : '';
-
-    console.log(targetStatusIssues);
-    console.log('Before:', destination.index - 1, indexBeforeTarget);
-    console.log('Target:', destination.index, indexOnTarget);
-
-    if (destination.droppableId === source.droppableId) {
-      let newIndex = '';
-      if (indexBeforeTarget === '') {
-        console.log('První');
-        newIndex = LexoRank.parse(indexOnTarget).genPrev().toString();
-      } else if (indexNextTarget === '') {
-        console.log('Poslední');
-        newIndex = LexoRank.parse(indexOnTarget).genNext().toString();
-      } else {
-        //FIXME: need to check if moved up or down to get the index
-        newIndex = LexoRank.parse(indexOnTarget)
-          .between(LexoRank.parse(indexNextTarget))
-          .toString();
-      }
-
-      updateIssue({
-        variables: {
-          projectId,
-          issueId: draggableId,
-          index: newIndex,
-        },
-      });
-      return;
-    }
-    console.log('Změna statusu');
+  const handleDragEnd = (result: DropResult) => {
+    reorderIssue(result);
   };
 
   return (
@@ -124,16 +56,9 @@ const Board = ({ projectId }: IBoardProps) => {
             {statusList.map((status: string) => (
               <List
                 key={status}
-                issues={
-                  // issues
-                  //   ? issues.filter((issue: IIssue) => issue.status === status)
-                  //   : cachedIssues.getIssues.filter(
-                  //       (issue: IIssue) => issue.status === status
-                  //     )
-                  cachedIssues.getIssues.filter(
-                    (issue: IIssue) => issue.status === status
-                  )
-                }
+                issues={cachedIssues.getIssues.filter(
+                  (issue: IIssue) => issue.status === status
+                )}
                 status={status}
               />
             ))}
---
+++ client/src/utils/useReorderHook.tsx
@@ -0,0 +1,134 @@
+import { ApolloError, gql, useApolloClient, useMutation } from '@apollo/client';
+import { DropResult } from 'react-beautiful-dnd';
+import { GET_ISSUES } from '../graphql/issuesQuery';
+import { LexoRank } from 'lexorank';
+import { UPDATE_BOARD_ISSUE } from '../graphql/issuesMutation';
+
+const useReorderHook = (projectId: string) => {
+  const client = useApolloClient();
+
+  const [updateIssue] = useMutation(UPDATE_BOARD_ISSUE, {
+    onError(err: ApolloError) {
+      console.log(err);
+    },
+  });
+
+  const cachedIssues = client.readQuery({
+    query: GET_ISSUES,
+    variables: {
+      projectId,
+    },
+  });
+
+  const reorderIssue = ({ source, destination, draggableId }: DropResult) => {
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    const targetStatusIssues =
+      cachedIssues.getIssues.length !== 0
+        ? cachedIssues.getIssues
+            .filter((issue: any) => issue.status === destination.droppableId)
+            .sort((a: any, b: any) => {
+              if (a.index > b.index) return 1;
+              return -1;
+            })
+        : '';
+
+    const indexOnTarget = targetStatusIssues[destination.index]
+      ? targetStatusIssues[destination.index].index
+      : '';
+
+    const indexBeforeTarget = targetStatusIssues[destination.index - 1]
+      ? targetStatusIssues[destination.index - 1].index
+      : '';
+
+    const indexNextTarget = targetStatusIssues[destination.index + 1]
+      ? targetStatusIssues[destination.index + 1].index
+      : '';
+
+    if (destination.droppableId === source.droppableId) {
+      let newIndex = '';
+      if (indexBeforeTarget === '') {
+        newIndex = LexoRank.parse(indexOnTarget).genPrev().toString();
+      } else if (indexNextTarget === '') {
+        newIndex = LexoRank.parse(indexOnTarget).genNext().toString();
+      } else if (source.index > destination.index) {
+        newIndex = LexoRank.parse(indexBeforeTarget)
+          .between(LexoRank.parse(indexOnTarget))
+          .toString();
+      } else {
+        newIndex = LexoRank.parse(indexOnTarget)
+          .between(LexoRank.parse(indexNextTarget))
+          .toString();
+      }
+
+      client.writeFragment({
+        id: `Issue:${draggableId}`,
+        fragment: gql`
+          fragment IssueIndexPart on Issue {
+            index
+          }
+        `,
+        data: {
+          index: newIndex,
+        },
+      });
+      updateIssue({
+        variables: {
+          projectId,
+          issueId: draggableId,
+          index: newIndex,
+        },
+      });
+      return;
+    }
+    let newIndex = '';
+
+    if (
+      indexOnTarget === '' &&
+      indexBeforeTarget === '' &&
+      indexNextTarget === ''
+    ) {
+      newIndex = LexoRank.middle().toString();
+    } else if (indexBeforeTarget === '') {
+      newIndex = LexoRank.parse(indexOnTarget).genPrev().toString();
+    } else if (indexOnTarget === '' && indexNextTarget === '') {
+      newIndex = LexoRank.parse(indexBeforeTarget).genNext().toString();
+    } else {
+      newIndex = LexoRank.parse(indexBeforeTarget)
+        .between(LexoRank.parse(indexOnTarget))
+        .toString();
+    }
+
+    client.writeFragment({
+      id: `Issue:${draggableId}`,
+      fragment: gql`
+        fragment IssueIndexStatusPart on Issue {
+          index
+          status
+        }
+      `,
+      data: {
+        index: newIndex,
+        status: destination.droppableId,
+      },
+    });
+
+    updateIssue({
+      variables: {
+        projectId,
+        issueId: draggableId,
+        index: newIndex,
+        status: destination.droppableId,
+      },
+    });
+    return;
+  };
+
+  return { reorderIssue };
+};
+export default useReorderHook;
